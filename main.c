e1ecc4c78f98442570d4b5205c6b2f1538fbf730
PolitechnikaMM <149075916+PolitechnikaMM@users.noreply.github.com>
2024-07-04 21:35:13 +0200

    02 Zmiana metody policz (odejmowanie)

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int policz(int a, int b) {
+    return a-b;
+}
+
 int main(void) {
     printf("Hello, World!\n");
+    printf("Druga zmiana branch\n");
+    printf("2-3=%d", policz(2, 3));
     return 0;
 }
